31dd824630744915ddbad7f37b7c9a4d8196a3a9
Jaume Plomer <jaumeplomer@paucasesnovescifp.cat>
2020-02-19 12:11:43 +0100

    Segona versio PPTLS

--- PPT.java
+++ PPT.java
@@ -44,14 +44,38 @@ public class PPT
 					case 5: System.out.println("Guanyes!"); break;
 				}
 				break;
+
+			case 4:
+				System.out.println("Lagarto");
+				switch(tiradaUsuari)
+				{
+					case 1: System.out.println("Guanyes!"); break;
+					case 2: System.out.println("Perds!"); break;
+					case 3: System.out.println("Guanyes!"); break;
+					case 4: System.out.println("Empat!"); break;
+					case 5: System.out.println("Perds!"); break;
+				}
+				break;
+
+			case 5:
+				System.out.println("Spock");
+				switch(tiradaUsuari)
+				{
+					case 1: System.out.println("Perds!"); break;
+					case 2: System.out.println("Guanyes!"); break;
+					case 3: System.out.println("Perds!"); break;
+					case 4: System.out.println("Guanyes!"); break;
+					case 5: System.out.println("Empat!"); break;
+				}
+				break;
 		}
 	}
 	public static void main(String[] args)
 	{	
-		System.out.println("Aquest es el joc de pedra-paper-tissores");
+		System.out.println("Aquest es el joc de pedra-paper-tissores-lagarto-spock");
 		Scanner teclat = new Scanner(System.in);
 /*----------------------------------------------------*/
-		tiradaMaquina = (int)(Math.random() * 3) + 1;
+		tiradaMaquina = (int)(Math.random() * 5) + 1;
 		System.out.println("La maquina ja ha triat, i te guanyara");
 /*------------------------------------------------------*/
 		System.out.println("Tria: [1=Pedra, 2=Paper, 3=Tissores, 4=Lagarto, 5=Spock]: ");
